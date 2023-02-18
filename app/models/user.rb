13930f9572ee72fc69dd9650efef60685d1fc7fb
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2023-02-18 22:50:19 +0700

    Remove redundant Devise controllers and add jQuery.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+# User business logic
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :keyword_file
+  has_one_attached :keyword
 end
