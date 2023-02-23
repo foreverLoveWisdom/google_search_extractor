ce89b53cfd746575466c4deb892a890dfe30dc48
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2023-02-23 22:50:19 +0700

    Remove redundant code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protected
-
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to new_user_registration_path, notice: 'You need to sign up before continuing.'
-    end
-  end
 end
